6cf717a1e77f7925472682f52290af3359cea9aa
pythondb <ayushkumar@gmail.com>
2026-05-18 19:17:42 +0530

    video change

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,9 @@
 import { Route, Routes } from "react-router-dom";
 import Navbar from "./Components/Navbar";
-import Dashboard from "./Components/Dashboard";
 import TreeData from "./Components/TreeData";
 import Demonstration from "./Components/Demonstration";
 import CircuitDiagram from "./Components/CircuitDiagram";
 import DetailsPlant from "./Components/DetailsPlant";
-import landing from "./Components/Landing";
 import Landing from "./Components/Landing";
 
 function App() {
